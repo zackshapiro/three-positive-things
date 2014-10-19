e8172dd46247b32fcfd610255538ef0cfd5f4f25
Zack Shapiro <zack@59thirty.com>
2014-10-19 12:32:22 -0700

    Initial Commit

---
+++ Three Positive Things/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Three Positive Things
+//
+//  Created by Zack Shapiro on 10/19/14.
+//  Copyright (c) 2014 com.59thirty.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
