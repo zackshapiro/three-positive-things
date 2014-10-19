eef56d9b9a5080113bb22910753e128a530bb6ef
Zack Shapiro <zack@59thirty.com>
2014-10-19 12:43:33 -0700

    first commit, now with coredata

--- Three Positive Things/ViewController.swift
+++ Three Positive Things/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var thirdField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var serverReturnLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitButtonPressed(sender: UIButton) {
+        resignFirstResponder()
+        submitButton.setTitle("Sending...", forState: UIControlState.Normal)
+        
+        var first = firstField.text
+        var second = secondField.text
+        var third = thirdField.text
+        
+        var obj = [
+            "first": first,
+            "second": second,
+            "third": third
+        ]
+        
+        println(obj)
+        
+        // if coredata responds with a 200, unhide the serverReturnLabel
+        // and show green text with a message of "your entry has been recorded"
+        // else, red label, message "there was a problem saving your entry"
+        
+        // button setTitle back to 'Send'
+        
+        // nice to have: don't let the user submit if any field is blank
+    }
 }
 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Three Positive Things
+A simple Swift app for writing three good things that happened to you each day.
+
+Studies show that recording three positive things each day leads to an increase overall happiness. Why? Because your brain begins to scan your day for good moments instead of bad moments. This simple exercise has had profound impact for many. 
+
+*Coming soon to an App Store near you.*
+
+Created by [Zack Shapiro](https://twitter.com/zackshapiro) and [Jasdev Singh](https://twitter.com/jasdev).
+
+Inspired by [Lift](http://lift.do) and the Lift community of over 10,500 people writing three positive things about their day each day.
